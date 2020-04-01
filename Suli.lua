42e041ea1645d265b3907f939fb82b7d943a25d3
DatoZuro <davidzuroshvili@davids-MacBook-Pro.local>
2020-04-01 22:05:40 +0400

    Added Search Feature

--- Suli.lua
+++ Suli.lua
@@ -10,12 +10,22 @@ Client:on("ready", function()
     local ServerArray = {}
     for k, v in pairs(Client.guilds) do
         table.insert(ServerArray,k)
-        SQLCOMMAND = "CREATE TABLE IF NOT EXISTS '" .. k .. "' (Word TEXT, Position INTEGER, Amount INTEGER, Before TEXT, After TEXT)"
+        SQLCOMMAND = "CREATE TABLE IF NOT EXISTS '" .. k .. "' (Word TEXT, Position TEXT, Amount TEXT, Before TEXT, After TEXT)"
         db:exec(SQLCOMMAND)
     end
     
     
 end)
+
+
+function getKeysSortedByValue(tbl, sortFunction)
+    local keys = {}
+    for key in pairs(tbl) do
+        table.insert(keys, key)
+    end
+    table.sort(keys, function(a, b) return sortFunction(tbl[a], tbl[b]) end)
+    return keys
+end
 
 Client:on("messageCreate", function(message)
     if(string.sub(message.content,1,1) ~= "[") then
@@ -51,7 +61,7 @@ Client:on("messageCreate", function(message)
 end)
 
 Client:on("messageCreate", function(message)
-    if(string.lower(string.sub(message.content,2,16)) == "randomsentence" and string.sub(message.content,1,1) == "[") then
+    if(string.lower(string.sub(message.content,2,#message.content)) == "randomsentence" and string.sub(message.content,1,1) == "[") then
         local Seed = math.random()
         print(Seed)
         math.randomseed(Seed)
@@ -103,10 +113,11 @@ Client:on("messageCreate", function(message)
             local Pot = {}
             for m=1, table.getn(Candidates) do
                 --print(Candidates[m])
-                Pot[Candidates[m]] = Checkpoint + Amountlist[Candidates[m]]
+                Pot[Candidates[m]] = Checkpoint + tonumber(Amountlist[Candidates[m]])
                 --print(Candidates[m] .. " " .. Pot[Candidates[m]] .. " " .. Checkpoint .. " " .. Amountlist[Candidates[m]])
-                Checkpoint = Checkpoint + Amountlist[Candidates[m]]
+                Checkpoint = Checkpoint + tonumber(Amountlist[Candidates[m]])
             end
+            print("CheckPoint :" .. Checkpoint)
             local THEONE = math.random(1,Checkpoint)
             for m,n in ipairs(Pot) do 
                 print(m,n)
@@ -130,4 +141,237 @@ Client:on("messageCreate", function(message)
     end
 end)
 
-Client:run("Bot ")
+
+Client:on("messageCreate", function(message)
+    if(string.sub(message.content,1,1) == "[" and string.lower(string.sub(message.content,2,9)) == "sentence") then
+        local TargetWord = string.lower(string.sub(message.content,11,#message.content))
+        local isWordinTheDB = 0
+        local ChosenPosition,ChosenPositionIndex,ChosenWord,ChosenWordIndex
+        local Sentence = {}
+
+        local Before
+        local After
+        
+        SQLCOMMAND = "SELECT * from '" .. message.channel.guild.id .. "' Where Word='" .. TargetWord .. "'"
+        local Rows,errorString = db:exec(SQLCOMMAND)
+        if(errorString == 0) then
+            message.channel:send("ERROR404")
+            isWordinTheDB = 1
+        else
+            local Positionlist = {}
+            local Amountlist = {}
+            local Beforelist = {}
+            local Afterlist = {}
+
+            for m,n in pairs(Rows) do
+                if(m == "Amount") then
+                    Amountlist = n
+                end
+                if(m == "Position") then
+                    Positionlist = n
+                end
+                if(m == "Before") then
+                    Beforelist = n
+                end
+                if(m == "After") then
+                    Afterlist = n
+                end
+            end
+
+            local PositionValues = {}
+            for m=1, table.getn(Positionlist) do
+                --print(TargetWord .. ":" .. Positionlist[m] .. ":" .. Amountlist[m] .. ":" .. Beforelist[m] .. ":" .. Afterlist[m])
+                if(PositionValues[tonumber(Positionlist[m])] == nil) then
+                    PositionValues[tonumber(Positionlist[m])] = tonumber(Amountlist[m])
+                    --print(PositionValues[tonumber(Positionlist[m])])
+                    --print(tonumber(Positionlist[m]))
+                    --print(m)
+                else
+                    PositionValues[tonumber(Positionlist[m])] = PositionValues[tonumber(Positionlist[m])] + tonumber(Amountlist[m])
+                end
+            end
+            local sortedPositions = {}
+            for k, v in pairs(PositionValues) do
+                --print(k,v)
+                table.insert(sortedPositions,{k,v})
+            end
+            table.sort(sortedPositions, function(a,b) return a[2] < b[2] end)
+            local PositionPot = {}
+            local Checkpoint = 0
+            for m,n in ipairs(sortedPositions) do
+                --print(n[1],n[2])
+                PositionPot[n[1]] = Checkpoint + n[2]
+                Checkpoint = PositionPot[n[1]]
+            end
+            local sortedPositionsPot = {}
+            for k, v in pairs(PositionPot) do
+                --print(k,v)
+                table.insert(sortedPositionsPot,{k,v})
+            end
+            table.sort(sortedPositionsPot, function(a,b) return a[2] < b[2] end)
+            local THEONE = math.random(1,Checkpoint)
+            for m,n in ipairs(sortedPositionsPot) do
+                --print(n[1],n[2],THEONE)
+                if(n[2] >= THEONE) then
+                    ChosenPosition = n[1]
+                    ChosenPositionIndex = n[1]
+                    break
+                end
+            end
+        end
+        if(isWordinTheDB == 0) then
+            SQLCOMMAND = "SELECT * from '" .. message.channel.guild.id .. "' Where Word='" .. TargetWord .. "' AND Position='" .. ChosenPosition .. "'"
+            local Rows,errorString = db:exec(SQLCOMMAND)
+            if(errorString == 0) then
+                print("WTF CODE 3")
+            else
+                local Amountlist = {}
+                local Beforelist = {}
+                local Afterlist = {}
+
+                for m,n in pairs(Rows) do
+                    if(m == "Amount") then
+                        Amountlist = n
+                    end
+                    if(m == "Before") then
+                        Beforelist = n
+                    end
+                    if(m == "After") then
+                        Afterlist = n
+                    end
+                end
+                local WordPot = {}
+                local Checkpoint = 0
+                for m=1, table.getn(Amountlist) do
+                    --print(TargetWord .. ":" .. ChosenPosition .. ":" .. Amountlist[m] .. ":" .. Beforelist[m] .. ":" .. Afterlist[m])
+                    WordPot[m] = Checkpoint + tonumber(Amountlist[m])
+                    Checkpoint = WordPot[m]
+                end
+
+                local THEONE = math.random(1,Checkpoint)
+                for m,n in ipairs(WordPot) do
+                    --print(m,n)
+                    if(n >= THEONE) then
+                        ChosenWord = m
+                        ChosenWordIndex = m
+                        Before = Beforelist[ChosenWordIndex]
+                        After = Afterlist[ChosenWordIndex]
+                        break
+                    end
+                end
+                print("CHOSEN WORD:" .. TargetWord .. ":" .. ChosenPosition .. ":" .. Amountlist[ChosenWordIndex] .. ":" .. Before .. ":" .. After)
+            end
+            Sentence[ChosenPosition] = TargetWord
+            --print("TEST :" .. Sentence[ChosenPosition])
+
+            for k=ChosenPosition-1,1,-1 do
+                SQLCOMMAND="SELECT * from '" .. message.channel.guild.id .. "' Where Word='" .. Before .. "' AND Position='" .. k .. "'"
+                print(SQLCOMMAND)
+                local Rows,errorString = db:exec(SQLCOMMAND)
+                if(errorString == 0) then
+                    print("WTF CODE 4")
+                else
+                    local Amountlist = {}
+                    local Beforelist = {}
+                    local Afterlist = {}
+                    local tmpBefore,tmpAfter
+    
+                    for m,n in pairs(Rows) do
+                        if(m == "Amount") then
+                            Amountlist = n
+                        end
+                        if(m == "Before") then
+                            Beforelist = n
+                        end
+                        if(m == "After") then
+                            Afterlist = n
+                        end
+                    end
+                    local WordPot = {}
+                    local Checkpoint = 0
+                    for m=1, table.getn(Amountlist) do
+                        --print(TargetWord .. ":" .. ChosenPosition .. ":" .. Amountlist[m] .. ":" .. Beforelist[m] .. ":" .. Afterlist[m])
+                        WordPot[m] = Checkpoint + tonumber(Amountlist[m])
+                        Checkpoint = WordPot[m]
+                    end
+                
+                    local THEONE = math.random(1,Checkpoint)
+                    for m,n in ipairs(WordPot) do
+                        --print(m,n)
+                        if(n >= THEONE) then
+                            Sentence[k] = Before
+                            --print("BEFORE : " .. Before)
+                            tmpBefore = Beforelist[m]
+                            tmpAfter = Afterlist[m]
+                            break
+                        end
+                    end
+                    Before = tmpBefore
+                    After = tmpAfter
+                    --print(Sentence[k] .. ":" .. k .. ":" .. Before .. ":" .. After)
+                end
+            end
+            local k=ChosenPosition+1
+            while After ~= "FRONTEND" do
+                SQLCOMMAND="SELECT * from '" .. message.channel.guild.id .. "' Where Word='" .. After .. "' AND Position='" .. k .. "'"
+                print(SQLCOMMAND)
+                local Rows,errorString = db:exec(SQLCOMMAND)
+                if(errorString == 0) then
+                    print("WTF CODE 5")
+                    break
+                else
+                    local Amountlist = {}
+                    local Beforelist = {}
+                    local Afterlist = {}
+                    local tmpBefore,tmpAfter
+                
+                    for m,n in pairs(Rows) do
+                        if(m == "Amount") then
+                            Amountlist = n
+                        end
+                        if(m == "Before") then
+                            Beforelist = n
+                        end
+                        if(m == "After") then
+                            Afterlist = n
+                        end
+                    end
+                    local WordPot = {}
+                    local Checkpoint = 0
+                    for m=1, table.getn(Amountlist) do
+                        --print(TargetWord .. ":" .. ChosenPosition .. ":" .. Amountlist[m] .. ":" .. Beforelist[m] .. ":" .. Afterlist[m])
+                        WordPot[m] = Checkpoint + tonumber(Amountlist[m])
+                        Checkpoint = WordPot[m]
+                    end
+                
+                    local THEONE = math.random(1,Checkpoint)
+                    for m,n in ipairs(WordPot) do
+                        --print(m,n)
+                        if(n >= THEONE) then
+                            Sentence[k] = After
+                            --print("After : " .. After)
+                            tmpBefore = Beforelist[m]
+                            tmpAfter = Afterlist[m]
+                            break
+                        end
+                    end
+                    Before = tmpBefore
+                    After = tmpAfter
+                    --print(Sentence[k] .. ":" .. k .. ":" .. Before .. ":" .. After)
+                end
+                k=k+1
+            end
+
+            local SentenceString = ""
+            --print(table.getn(Sentence))
+            for m,n in pairs(Sentence) do 
+                --print(m,n)
+                SentenceString = SentenceString .. Sentence[m] .. " "
+            end 
+            print("SENTENCE:" .. SentenceString)
+            message.channel:send(SentenceString)
+        end
+    end
+end)
+
+Client:run("Bot NjkzNDQ4MTUyMDY5NzAxNjUy.XoOdsw.gR6xm45Z9mEVlnb2UJ7pc81iMH8")
